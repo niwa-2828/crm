1889d2ee795593df5f60ccbd301906830c5c8437
niwa <khn.kazmasa@gmail.com>
2024-01-03 21:38:15 +0900

    42-44

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Item;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
-
+use Inertia\Inertia;
 class ItemController extends Controller
 {
     /**
@@ -15,7 +15,12 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+
+        $items=(Item::select('id','name','price','is_selling')->get());
+
+        return Inertia::render('Items/Index',[
+            "items" => $items
+        ]);
     }
 
     /**
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,6 +32,9 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+                                <NavLink :href="route('items.index')" :active="route().current('items.index')">
+                                    商品管理
+                                </NavLink>
                             </div>
                         </div>
 
@@ -115,6 +118,9 @@ const showingNavigationDropdown = ref(false);
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
                         </ResponsiveNavLink>
+                        <NavLink :href="route('items.index')" :active="route().current('items.index')">
+                                    商品管理
+                        </NavLink>
                     </div>
 
                     <!-- Responsive Settings Options -->
